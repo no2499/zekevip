993ec44dc48d48efb7d49534114080b8e49a4adf
no2499 <ihaveno2499@gmail.com>
2023-03-19 01:40:02 +0800

    rename

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ze BLOG';
-export const SITE_DESCRIPTION = 'Ze BLOG.';
+export const SITE_TITLE = '傷心藥水 [ze]';
+export const SITE_DESCRIPTION = '傷心藥水';
